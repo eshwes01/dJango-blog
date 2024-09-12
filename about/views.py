ec902f555fe4cc2d5ef65f1e69e02771743b7235
Ei Sin <einge174@gmail.com>
2024-09-12 14:51:08 +0000

    adbug fix for about_me function

--- about/views.py
+++ about/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import About
 
 # Create your views here.
+def about_me(request):
+    about = About.objects.all().order_by('-updated_on').first()
+    template_name = "about.html"
+    context = {"about": about}
+    return render(
+        request,
+        "about/about.html",
+        context
+    )
--- codestar/urls.py
+++ codestar/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("blog.urls"), name="blog-urls"),
     path('summernote/', include('django_summernote.urls')),
-    path("about/", include("about.urls"), name="about-urls"),
+    path('about/', include("about.urls"), name="about-urls"),
 ]
