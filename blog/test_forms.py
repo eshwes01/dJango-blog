14b6610ea4bda522945cc7e6f50c01c94756293f
Ei Sin <einge174@gmail.com>
2024-09-17 14:04:47 +0000

    cadd code to switch DB

--- blog/test_forms.py
+++ blog/test_forms.py
@@ -1,6 +1,5 @@
 from django.test import TestCase
 from .forms import CommentForm
-from .forms import CollaborateForm
 
 # Create your tests here.
 class TestCommentForm(TestCase):
@@ -12,39 +11,3 @@ class TestCommentForm(TestCase):
     def test_form_is_invalid(self):
         comment_form = CommentForm({'body': ''})
         self.assertFalse(comment_form.is_valid(), msg="Form is valid")
-
-    def test_name_is_required(self):
-        """Test for the 'name' field"""
-        form = CollaborateForm({
-            'name': 'Test',
-            'email': 'test@test.com',
-            'message': 'Hello!'
-        })
-        self.assertFalse(
-            form.is_valid(),
-            msg="Name was not provided, but the form is valid"
-        )
-
-    def test_email_is_required(self):
-        """Test for the 'email' field"""
-        form = CollaborateForm({
-            'name': 'Matt',
-            'email': '',
-            'message': 'Hello!'
-        })
-        self.assertFalse(
-            form.is_valid(),
-            msg="Email was not provided, but the form is valid"
-        )
-
-    def test_message_is_required(self):
-        """Test for the 'message' field"""
-        form = CollaborateForm({
-            'name': 'Matt',
-            'email': 'test@test.com',
-            'message': ''
-        })
-        self.assertFalse(
-            form.is_valid(),
-            msg="Message was not provided, but the form is valid"
-        )
